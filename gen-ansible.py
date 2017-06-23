8bc9f79a9c76a10a18b70bb9371c877bab630158
Gowtham k <sanju@Gowthams-MacBook-Pro.local>
2017-06-22 20:39:03 -0400

    added yaml support.

--- gen-ansible.py
+++ gen-ansible.py
@@ -5,6 +5,9 @@ Created on Jun 21, 2017
 '''
 import logging
 from string import lower
+import yaml, json
+import pprint
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -19,7 +22,7 @@ handler.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(handler)
 
-DEFAULT_USER = 'admin'
+DEFAULT_USER = 'admin@local'
 DEFAULT_PASS = 'admin'
 DEFAULT_PORT = '9083'
 DEFAULT_HOSTNAME = '192.168.92.67'
@@ -30,27 +33,35 @@ delete_runtime_interface_port = '8080'
 DEFAULT_LOG_LEVEL = 'DEBUG'
 force = True
 action = 'DELETE'
+isam_config_file = './isam.yml'
+isam_config_file_json = './isam.json'
 
 
 def create_isam_config_file():
-    isam_config_file = './isam.yml'
     logger.info('Creating %s', isam_config_file)
+    arr = []
+    data={}
+    data['name'] = 'Delete Runtime Tuning Parameters'
+    data['isam'] = {}
+    data['isam']['appliance'] = DEFAULT_HOSTNAME
+    data['isam']['username'] = DEFAULT_USER
+    data['isam']['password'] = DEFAULT_PASS
+    data['isam']['lmi_port'] = DEFAULT_PORT
+    data['isam']['log'] = DEFAULT_LOG_LEVEL
+    data['isam']['force'] = force
+    data['isam']['action'] = get_action(action)
+    data['isam']['isamapi']={}
+    data['isam']['isamapi']['interface'] = delete_runtime_interface_interface
+    data['isam']['isamapi']['port'] = delete_runtime_interface_port
+    data['when'] = 'delete_runtime_interface_interface is defined and delete_runtime_interface_port is defined'
+    data['notify'] = 'Commit Changes'
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(data)
+    arr.append(data)
+    logger.info('Final yaml \n %s', yaml.dump(arr,default_flow_style=False))
+
     with open(isam_config_file, 'w+') as f:
-#         f.write('  {kernel, [{net_ticktime,  %s}]},\n' % net_ticktime)
-        f.write('- name: Delete Runtime Tuning Parameters\n')
-        f.write('  isam:\n')
-        f.write('    appliance: "%s"\n'  % DEFAULT_HOSTNAME)
-        f.write('    username: "%s"\n'  % DEFAULT_USER)
-        f.write('    password: "%s"\n'  % DEFAULT_PASS)
-        f.write('    lmi_port: "%s"\n'  % DEFAULT_PORT)
-        f.write('    log: "%s"\n'  % DEFAULT_LOG_LEVEL)
-        f.write('    force: "%s"\n'  % force)
-        f.write('    action: %s\n'  % get_action(action))
-        f.write('    isamapi:\n')
-        f.write('      interface:  "%s"\n'  % delete_runtime_interface_interface)
-        f.write('      port:       "%s"\n'  % delete_runtime_interface_port)
-        f.write('  when: delete_runtime_interface_interface is defined and delete_runtime_interface_port is defined \n')
-        f.write('  notify: Commit Changes')
+        yaml.safe_dump(arr,f, allow_unicode=True, encoding='utf-8', default_flow_style=False, indent=4)
 
 def get_action(method):
     switcher = {
@@ -62,4 +73,6 @@ def get_action(method):
 
 if __name__ == '__main__':
 
+    logger.info('>> create_isam_config_file')
     create_isam_config_file()
+    logger.info('<< create_isam_config_file')
